783fcf35d6c150165d60f60f8a33c261c4bc1865
LasterSmithKim <kingone@yeah.net>
2020-06-09 20:38:47 +0800

    Initial commit

---
+++ app/src/main/java/com/smithkim/android_listview/MainActivity.java
@@ -0,0 +1,59 @@
+package com.smithkim.android_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] from = {"title"};
+    private int[] to = {R.id.item_title};
+    private LinkedList<HashMap<String,String>> data = new LinkedList<>();
+    private SimpleAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+        initListView();
+    }
+    private void initListView(){
+
+
+        HashMap<String,String> d0 = new HashMap<>();
+        d0.put(from[0],"Test1");
+        data.add(d0);
+
+        HashMap<String,String> d1 = new HashMap<>();
+        d1.put(from[0],"Test2");
+        data.add(d1);
+
+        HashMap<String,String> d2 = new HashMap<>();
+        d2.put(from[0],"Test3");
+        data.add(d2);
+
+        adapter = new SimpleAdapter(this,data,R.layout.item,from,to);
+        listView.setAdapter(adapter);
+
+    }
+
+    public void del(View view) {
+        if(data.size()>0){
+            data.remove(0);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    public void add(View view) {
+        HashMap<String,String> d3 = new HashMap<>();
+        d3.put(from[0],"Test4");
+        data.add(d3);
+        adapter.notifyDataSetChanged();
+    }
+}
